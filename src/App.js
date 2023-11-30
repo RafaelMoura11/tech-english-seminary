6426e654193b712f20d82d57c56a4d52c14ff409
Rafael <rafaelmoura7033@gmail.com>
2023-11-30 12:03:57 -0300

    Last section

--- src/App.js
+++ src/App.js
@@ -17,10 +17,10 @@ function App() {
   return (
     <div id="main">
       <div id="first-section">
-        <img src="https://www.pe.senac.br/wp-content/uploads/2018/05/Professor_Saulo-Bernardo-1.jpg" alt="professor-saulo" id="saulo-image"/>
+        <img src="https://i1.wp.com/blog.alexdevero.com/wp-content/uploads/2015/02/css-code-e1517469545230.jpeg?zoom=2&resize=1008%2C672&ssl=1" alt="css-image" id="css-image"/>
         <div id="hello-world">
           <h1>Hello World</h1>
-          <p>With CSS you can make your page much more organized and prettier</p>
+          <p>With CSS you can make your page much more organized and prettier. You can do anything you want with your HTML tags.</p>
         </div>
       </div>
 
@@ -41,10 +41,33 @@ function App() {
       <div id="third-section">
         <div className="animated-div" id="change-color-div">Changing the color!</div>
         <div className="animated-div" id="spinning-div">Spinning around!</div>
-        <div className="animated-div" id="change-size-div">Put the mouse over me!</div>
+        <div className="animated-div" id="change-size-div">I want to be bigger!!</div>
       </div>
 
       <hr />
+
+      <div id="last-section">
+        <div>
+          <img src="https://media-for1-1.cdn.whatsapp.net/v/t61.24694-24/322101539_209088515188259_6663516498777055739_n.jpg?ccb=11-4&oh=01_AdRJL_b_Fk8E43uy70EaCzbn6f1laF-3BufRJ21l5ktstA&oe=656F5995&_nc_sid=e6ed6c&_nc_cat=101" alt="klara" />
+          <p>Klara</p>
+        </div>
+        <div>
+          <img src="https://media-for1-1.cdn.whatsapp.net/v/t61.24694-24/352989230_781288370130028_7065883434391183806_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdQdNzon6XuQzIn7bbTyzndJxt6_INOOy7OG5SI6mQrQmw&oe=6575AA41&_nc_sid=e6ed6c&_nc_cat=104" alt="rafael" />
+          <p>Rafael</p>
+        </div>
+        <div>
+          <img src="https://media-for1-1.cdn.whatsapp.net/v/t61.24694-24/348531552_201729159441975_9151658873640286015_n.jpg?ccb=11-4&oh=01_AdRww5MxW79PFAau1fxEzFL-woF4724RIxWTZlGFVyUTRQ&oe=656C9CC7&_nc_sid=e6ed6c&_nc_cat=101" alt="carlos-soares" />
+          <p>Carlos 1</p>
+        </div>
+        <div>
+          <img src="https://media-for1-1.cdn.whatsapp.net/v/t61.24694-24/402488652_365782826020165_2625997548177862065_n.jpg?ccb=11-4&oh=01_AdTtPvX6GEHfyq-QD4Yh2MCqbt0tFFlbnfzQszv6FO31bA&oe=6573A710&_nc_sid=e6ed6c&_nc_cat=108" alt="carlos-jr" />
+          <p>Carlos 2</p>
+        </div>
+        <div>
+          <img src="https://i.imgur.com/xzgJYG9.png" alt="helder" />
+          <p>Helder</p>
+        </div>
+      </div>
     </div>
   );
 }
